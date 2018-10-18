ec12452d520981d5e9354061819d056bbd7f45be
sergiioele <sergiioele@gmail.com>
2018-10-18 09:58:04 -0500

    Agrega tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-tareas',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-tareas';
+  tareas: Tarea[];//arreglo de tareas
+  ultimoId:number;
+
+  constructor(){
+    this.tareas=[{id:1,titulo:'Terminar el API',completada:false},{id:2, titulo:'Revisar historias de usuario', completada:true}, new Tarea( {id:3, titulo:'Comprar boletos para el cine'})];
+    this.ultimoId=3;
+  }
+   
+  agregarTarea(tituloTarea:string){
+    const tareaNueva = new Tarea({titulo:tituloTarea});
+    tareaNueva.id=++this.ultimoId;
+    this.tareas.push(tareaNueva);
+  }  
+}
+
+
+class Tarea{//clase en type script
+  id:number;
+  titulo:string;
+  completada:boolean;
+
+  constructor(valores:Object={}){
+    Object.assign(this,valores);
+  }
 }
